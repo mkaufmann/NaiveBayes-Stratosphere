2b2c299ece82b11d5637aadd4c1f8b9f01c448a1
Matthias Ringwald <matthias.ringwald@campus.tu-berlin.de>
2011-06-19 20:18:55 +0200

    Changed byetext to only wrap String

--- core/src/main/java/de/tu_berlin/dima/aim3/naivebayes/data/ByteText.java
+++ core/src/main/java/de/tu_berlin/dima/aim3/naivebayes/data/ByteText.java
@@ -8,10 +8,11 @@ import java.util.Arrays;
 import eu.stratosphere.pact.common.type.Key;
 
 public class ByteText implements Key {
-	byte[] value;
+	//byte[] value;
+	String value;
 	
 	public ByteText(byte[] bytes) {
-		this.value = bytes;
+		this.value = new String(bytes);
 	}
 	
 	public ByteText() {
@@ -20,39 +21,19 @@ public class ByteText implements Key {
 
 	@Override
 	public void read(DataInput in) throws IOException {
-		//value array is never allowed to be null!!
-		int length = in.readInt();
-		value = new byte[length];
-		in.readFully(value);
+		value = in.readUTF();  
 	}
 
 	@Override
 	public void write(DataOutput out) throws IOException {
-		//value array is never allowed to be null!!
-		out.writeInt(value.length);
-		out.write(value);
+		out.writeUTF(value);
 	}
 
 	@Override
 	public int compareTo(Key o) {
 		//Sorts by length first and than by content
-		ByteText other = (ByteText) o;
 		
-		//value array is never allowed to be null!!
-		int diff = value.length - other.value.length;
-		if(diff != 0) {
-			return diff;
-		}
-		else {
-			//Both have same length
-			for (int i = 0; i < value.length; i++) {
-				if(value[i] - other.value[i] != 0) {
-					return value[i] - other.value[i];
-				}
-			}
-		}
-		// TODO Auto-generated method stub
-		return 0;
+		return value.compareTo(((ByteText)o).value);
 	}
 	
 	@Override
@@ -62,24 +43,11 @@ public class ByteText implements Key {
 
 	@Override
 	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + Arrays.hashCode(value);
-		return result;
+		return value.hashCode();
 	}
 
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		
-		ByteText other = (ByteText) obj;
-		if (!Arrays.equals(value, other.value))
-			return false;
-		return true;
+		return value.equals(((ByteText)obj).value);
 	}
 }
